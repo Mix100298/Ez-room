7c03c6b2db65273c90fc550c6fa456a379c16b9a
Mix100298 <mix100298@gmail.com>
2024-02-01 18:55:08 +0700

    `update`: update dropdown filter

--- src/app/components/searchfilter.tsx
+++ src/app/components/searchfilter.tsx
@@ -1,24 +1,61 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 export default function Searchfilter() {
+  const [filter, setFilter] = useState(false);
+
   return (
     <div className="flex gap-10">
-      <input
-        type="text"
-        name="search"
-        id="search"
-        className="bg-white border border-gray-300 text-gray-700 sm:text-sm rounded w-full p-2.5"
-        placeholder="Search"
-        autoComplete="off"
-      ></input>
-      <input
-        type="text"
-        name="filter"
-        id="filter"
-        className="bg-white border border-gray-300 text-gray-700 sm:text-sm rounded w-full p-2.5"
-        placeholder="Filter"
-        autoComplete="off"
-      ></input>
+      <div className=" fill-slate-500 text-gray-700 w-full ">
+        <input
+          className="bg-white border border-gray-300 text-gray-700 h-10 w-full sm:text-sm rounded p-2.5"
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search"
+          autoComplete="off"
+        />
+      </div>
+      <div className="flex w-full">
+        <button
+          id="dropdown"
+          className="bg-white border w-full border-gray-300 text-gray-400 sm:text-sm rounded-md px-3 text-center inline-flex items-center justify-between"
+          onClick={() => setFilter(!filter)}
+          type="button"
+        >
+          Filter
+          <svg
+            className="fill-slate-500"
+            xmlns="http://www.w3.org/2000/svg"
+            width="12"
+            height="12"
+            viewBox="0 0 24 24"
+          >
+            <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" />
+          </svg>
+        </button>
+        {/* Dropdown menu */}
+        {filter && (
+          <div className="absolute mt-12 w-52 divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-gray-100">
+            <div className="py-1">
+              <a href="#" className="text-gray-700 block px-4 py-2 text-sm">
+                01
+              </a>
+              <a href="#" className="text-gray-700 block px-4 py-2 text-sm">
+                02
+              </a>
+            </div>
+            <div className="py-1">
+              <a href="#" className="text-gray-700 block px-4 py-2 text-sm">
+                03
+              </a>
+              <a href="#" className="text-gray-700 block px-4 py-2 text-sm">
+                04
+              </a>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
