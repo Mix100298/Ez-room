5ce5c4b1494ab6f425d83462b2c671aac4a3f776
Ball <ball.fiy555@gmail.com>
2024-03-07 13:59:22 +0700

    `feat` : create the ezroomContext and footer

---
+++ src/app/EZroomContext.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { useState, useEffect, createContext, useContext } from "react";
+
+const EZroomProvider = createContext({} as any);
+
+export function EZroomContext({ children }: any) {
+  const [info, setInfo] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [isLogged, setIsLogged] = useState(false);
+
+  useEffect(() => {
+    console.log("info", info);
+  }, [info]);
+
+  return (
+    <EZroomProvider.Provider
+      value={{
+        info,
+        setInfo,
+        isLoading,
+        setIsLoading,
+        error,
+        setError,
+        isLogged,
+        setIsLogged,
+      }}
+    >
+      {children}
+    </EZroomProvider.Provider>
+  );
+}
+
+export function useEZroom() {
+  return useContext(EZroomProvider);
+}
