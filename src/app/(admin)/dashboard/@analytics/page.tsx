6003ff80052504ecef0cb102afce7f4ecd37b721
unknown <mix100298@gmail.com>
2024-04-07 15:09:02 +0700

    `feat`: chart admin

--- src/app/(admin)/dashboard/@analytics/page.tsx
+++ src/app/(admin)/dashboard/@analytics/page.tsx
@@ -1,10 +1,47 @@
-
+"use client";
+import React from "react";
+import { PieChart } from "@mui/x-charts/PieChart";
+import { BarChart } from "@mui/x-charts/BarChart";
 
 export default function Page() {
-    return (
-        <div className = "rounded flex items-center justify-center w-full h-[300px] bg-white mb-10">
-            <h1 className="text-6xl font-bold">Analytics</h1>
+  const data1 = [
+    { label: "Group A", value: 2400 },
+    { label: "Group B", value: 4567 },
+    { label: "Group C", value: 1398 },
+    { label: "Group D", value: 9800 },
+    { label: "Group E", value: 3908 },
+    { label: "Group F", value: 4800 },
+  ];
+  const data2 = [
+    { label: "Group A", value: 400 },
+    { label: "Group B", value: 300 },
+    { label: "Group C", value: 300 },
+    { label: "Group D", value: 200 },
+    { label: "Group E", value: 278 },
+    { label: "Group F", value: 189 },
+  ];
+  return (
+    <div className="rounded flex min-w-[460px] min-h-full bg-white p-5 justify-center items-center">
+      <div className="grid w-full">
+        <div className="flex flex-wrap w-full">
+          <PieChart
+            series={[
+              {
+                data: data1,
+                innerRadius: 60,
+                outerRadius: 80,
+                cx: 300,
+              },
+              { data: data2, outerRadius: 80, cx: 100 },
+            ]}
+            height={250}
+            slotProps={{
+              legend: { hidden: true },
+            }}
+          />
         </div>
-        
-    )   
+        <h1 className="text-2xl font-bold text-gray-800 "></h1>
+      </div>
+    </div>
+  );
 }
--- src/app/(admin)/dashboard/@monitor/page.tsx
+++ src/app/(admin)/dashboard/@monitor/page.tsx
@@ -1,10 +1,23 @@
-
+"use client";
+import React from "react";
+import { BarChart } from "@mui/x-charts/BarChart";
 
 export default function Page() {
-    return (
-        <div className = "rounded flex items-center justify-center w-full h-[300px] bg-white mb-10">
-            <h1 className="text-6xl font-bold">Monitor</h1>
-        </div>
-        
-    )   
+  return (
+    <div className="rounded grid min-w-[460px] min-h-full bg-white mb-10 p-5">
+      <h1 className="text-2xl font-bold text-gray-800 ">Style</h1>
+      <p className="text-sm text-gray-400">most style user used.</p>
+      <BarChart
+        xAxis={[
+          {
+            scaleType: "band",
+            data: ["Modern", "Contemporary", "Bohemain"],
+          },
+        ]}
+        series={[{ data: [1250, 650, 1500] }]}
+        grid={{ horizontal: true }}
+        height={250}
+      />
+    </div>
+  );
 }
